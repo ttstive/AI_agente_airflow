27dbbc74e426fe4f5b2558a2989af81d22722ecd
Estevão Maia <estevao.maia@a.ucb.br>
2025-12-16 10:49:36 -0300

    adicionando dags e conexao com gemini 2.0 para criar agent

---
+++ dags/payment.py
@@ -0,0 +1,18 @@
+from airflow.decorators import task, dag
+from pendulum import datetime
+
+@dag(
+    start_date=datetime(2025,1,1),
+    schedule="@daily",
+    catchup=False
+    
+)
+
+
+def payment():
+    @task
+    def process():
+        print("oK")
+    process()
+
+payment()    
